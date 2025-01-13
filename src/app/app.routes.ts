6fc7383e8927430234743e6ce85a6a6b6a704fd7
Ahmed-Gamal-m <Ahmed.Gamal@microtec.com.eg>
2025-01-13 14:31:15 +0200

    add contact us c

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path :'contact',
+    component:ContactUsComponent
+}
+   
+
+];
